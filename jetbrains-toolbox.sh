50777c584f4ec418e252f32f2fd96a4d32fd6cea
Joshua England <jengland@ohalo.co>
2025-09-15 10:40:11 +0100

    Fix install script for toolbox 2.8.1
    Handle path changes in the distribution archive.

--- jetbrains-toolbox.sh
+++ jetbrains-toolbox.sh
@@ -4,7 +4,7 @@ set -e
 set -o pipefail
 
 TMP_DIR="/tmp"
-INSTALL_DIR="$HOME/.local/share/JetBrains/Toolbox/bin"
+INSTALL_DIR="$HOME/.local/share/JetBrains/Toolbox"
 SYMLINK_DIR="$HOME/.local/bin"
 
 echo "### INSTALL JETBRAINS TOOLBOX ###"
@@ -22,16 +22,16 @@ mkdir -p "$INSTALL_DIR"
 rm "$INSTALL_DIR/jetbrains-toolbox" 2>/dev/null || true
 tar -xzf "$TMP_DIR/$ARCHIVE_FILENAME" -C "$INSTALL_DIR" --strip-components=1
 rm "$TMP_DIR/$ARCHIVE_FILENAME"
-chmod +x "$INSTALL_DIR/jetbrains-toolbox"
+chmod +x "$INSTALL_DIR/bin/jetbrains-toolbox"
 
 echo -e "\e[94mSymlinking to $SYMLINK_DIR/jetbrains-toolbox...\e[39m"
 mkdir -p $SYMLINK_DIR
 rm "$SYMLINK_DIR/jetbrains-toolbox" 2>/dev/null || true
-ln -s "$INSTALL_DIR/jetbrains-toolbox" "$SYMLINK_DIR/jetbrains-toolbox"
+ln -s "$INSTALL_DIR/bin/jetbrains-toolbox" "$SYMLINK_DIR/jetbrains-toolbox"
 
 if [ -z "$CI" ]; then
 	echo -e "\e[94mRunning for the first time to set-up...\e[39m"
-	( "$INSTALL_DIR/jetbrains-toolbox" & )
+	( "$INSTALL_DIR/bin/jetbrains-toolbox" & )
 	echo -e "\n\e[32mDone! JetBrains Toolbox should now be running, in your application list, and you can run it in terminal as jetbrains-toolbox (ensure that $SYMLINK_DIR is on your PATH)\e[39m\n"
 else
 	echo -e "\n\e[32mDone! Running in a CI -- skipped launching the AppImage.\e[39m\n"
